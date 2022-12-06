4f8c2ef12cffab2d7cb09a589d1ead4c7ade78a0
Yashvi Singh <83403058+yashvi1234@users.noreply.github.com>
2022-12-06 15:59:25 +0530

    activity

---
+++ adapter/DashboardRecyclerAdapter.kt
@@ -0,0 +1,31 @@
+package com.yashvi.bookhub.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.yashvi.bookhub.R
+
+class DashboardRecyclerAdapter(val context: Context, val itemList: ArrayList<String>) : RecyclerView.Adapter<DashboardRecyclerAdapter.DashboardViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DashboardViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_dashboard_single_row, parent, false)
+
+        return DashboardViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+         return itemList.size
+    }
+
+    override fun onBindViewHolder(holder: DashboardViewHolder, position: Int) {
+        val text = itemList[position]
+        holder.textView.text = text
+    }
+
+    class DashboardViewHolder(view: View): RecyclerView.ViewHolder(view){
+        val textView: TextView = view.findViewById(R.id.txtRecyclerRowItem)
+    }
+}
